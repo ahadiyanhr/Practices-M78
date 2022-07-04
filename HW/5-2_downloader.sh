eeb18b17ffc0344d3185c0ede6c1700fb92ec8c7
hrahadiyan <ahadiyan.hr@gmail.com>
2022-07-04 22:40:33 +0430

    5-2 is compeleted

--- HW/5-2_downloader.sh
+++ HW/5-2_downloader.sh
@@ -4,6 +4,6 @@ echo $'-----------------------\nWelcome to my Bash-based Downloader Program!\nEn
 read url
 echo "Please Wait:"
 wget -c --show-progress -o log1.txt $url
-cat log1.txt | grep -w "saved" -m 1 > log.txt
+echo $'------\n'$(head -n 1 log1.txt)$'\n'$(tail -n 2 log1.txt) >> log.txt #Save result into log.txt
 rm log1.txt
 echo "The result saved in log.txt"
