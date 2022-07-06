334752c416e8dbabd469d65e0c7db7db16e742e3
hrahadiyan <ahadiyan.hr@gmail.com>
2022-07-06 20:23:36 +0430

    5-4 completed

--- HW/5-4.py
+++ HW/5-4.py
@@ -1,27 +1,12 @@
-class NextCall:
-    __num = 0
-    
-    def get_a(self):
-        return self.__num
-    def set_a(self, value):
-        NextCall.__num = value
-        
-    def __call__(self):
-        return self.__call__(NextCall.__num)
-
-    
-class Add(NextCall):
+class Add(int):
     def __init__(self, num1):
         self.num1 = num1
-        NextCall.__num = num1
         
     def __call__(self, num2):
-        NextCall.__num += num2
-        print(NextCall.__num)
-        NextCall.__call__(self)
-        
+        adds = self.num1 + num2
+        return Add(adds)
     
-print(Add(2)(4)(10))
+print(Add(2)(5)(19))
 
 
 
