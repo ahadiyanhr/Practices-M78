c3187319ab68bd4de6aed0c1850634d5d6aa0557
hrahadiyan <ahadiyan.hr@gmail.com>
2022-07-10 08:17:35 +0430

    feat: initialization and creation a null-matrix

--- HW/6-1.py
+++ HW/6-1.py
@@ -1,2 +1,22 @@
 class Matrix:
-    pass
+    '''
+    Matrix is a class for define a n*m Matrix by user.
+    '''
+    
+    def __init__(self, n: int, m: int) -> None:
+        self._n = n
+        self._m = m
+        self.create()
+        
+    def create(self) -> list:
+        '''
+        This function generates the n*m null-matrix.
+        '''
+        row = []
+        for i in range(self._n):
+            column = []
+            for j in range(self._m):
+                column.append(0)
+            row.append(column)
+        return row
+        
