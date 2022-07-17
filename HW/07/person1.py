9156f7b93f72e1e969689055f8281dd6a9244c9a
hrahadiyan <ahadiyan.hr@gmail.com>
2022-07-17 20:05:24 +0430

    feat: Q4 part 2 is coded.

---
+++ HW/07/person1.py
@@ -0,0 +1,31 @@
+import logging
+
+
+logging.basicConfig()
+log_file_format = "%(asctime)s — %(name)s — %(levelname)s — %(funcName)s:%(lineno)d — %(message)s"
+
+logging.basicConfig()
+file_handler = logging.FileHandler('person.log', 'a')
+log_format = logging.Formatter("%(asctime)s — %(name)s — %(levelname)s — %(message)s")
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(logging.Formatter(log_file_format))
+
+
+class Person():
+    def __init__(self, name, family, age):
+        self.name = name
+        self.family = family
+        self.age = age
+        
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, a):
+        if a > 0:
+            self._age = a
+            logging.info("Person created! {} {}".format(self.name, self.family))
+        else:
+            logging.error("Invalid age!")
+            self._age = 0
---
+++ HW/07/sample1.py
@@ -0,0 +1,32 @@
+from person1 import Person
+import logging
+
+
+log_file_format = "%(asctime)s — %(name)s — %(levelname)s — %(funcName)s:%(lineno)d — %(message)s"
+log_console_format = "%(asctime)s — %(levelname)s — %(message)s"
+
+logging.basicConfig()
+logger = logging.getLogger('my_logger')
+stream_handler = logging.StreamHandler()
+file_handler = logging.FileHandler('sample.log', 'a')
+stream_handler.setLevel(logging.ERROR)
+file_handler.setLevel(logging.INFO)
+stream_handler.setFormatter(logging.Formatter(log_console_format))
+file_handler.setFormatter(logging.Formatter(log_file_format))
+
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+
+def sub(a, b):
+    if b != 0:
+        logging.info("a/b=" + str(a / b))
+        return a / b    
+    logging.error("Divide by zero!")
+    
+
+
+print(sub(2, 3))
+print(sub(2, 0))
+
+p1 = Person("ali", "alavi", 23)
+p2 = Person("gholi", "gholami", -23)
