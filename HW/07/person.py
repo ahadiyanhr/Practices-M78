3b7c8f8fdbe7060974f39af6f0dd6eaf17052f9d
hrahadiyan <ahadiyan.hr@gmail.com>
2022-07-17 19:35:29 +0430

    refractor: Developer errors in code is corrected

--- HW/07/person.py
+++ HW/07/person.py
@@ -1,6 +1,8 @@
 import logging
 
-logging.basicConfig(level=logging.WARNING)
+# (*) The overall logging level is more than the body's log levels.
+# it is better to set the overall logging level to INFO to force code shows the all logs:
+logging.basicConfig(level=logging.INFO)
 
 
 class Person():
@@ -8,8 +10,7 @@ class Person():
         self.name = name
         self.family = family
         self.age = age
-        logging.warning("Person created! {} {}".format(self.name, self.family))
-
+        
     @property
     def age(self):
         return self._age
@@ -18,6 +19,16 @@ class Person():
     def age(self, a):
         if a > 0:
             self._age = a
+            
+            # This log relocate from __init__ to below line because it must run
+            # when instance created without any problem.
+            logging.info("Person created! {} {}".format(self.name, self.family))
         else:
-            logging.critical("Invalid age!")
-        self._age = 0
+            # (*) The logging.critical is more than needed for this kind of errors.
+            # because the code was unable to perform some function but it does not shut-down by that!!
+            # this log replace with logging.error:
+            logging.error("Invalid age!")
+            
+            # (*) {self._age = 0} must be in the scope of else statement as logging.critical
+            # in order to set negative ages to a non-negative value:
+            self._age = 0
--- HW/07/sample.py
+++ HW/07/sample.py
@@ -6,9 +6,19 @@ logging.basicConfig(level=logging.INFO)
 
 def sub(a, b):
     if b != 0:
-        return a / b
-        logging.debug("a/b=" + str(a / b))
+        # (*) The following debug log was unreachable in original file because was written after return.
+        # Then this log relocate for printing debug informations for developer.
+        
+        # (*) Moreover the DEBUG level replace with INFO because in this line, developer want to know
+        # about sub function is working as expected.
+        logging.info("a/b=" + str(a / b))
+        return a / b    
+    
+    # (*) It's better to raise ValueError for returning error in terminal, but in this case
+    # we ignore it because the file can't run after raise error, and p1&p2 Instances can not be instatiate
+    # from Person class.
     logging.error("Divide by zero!")
+    
 
 
 print(sub(2, 3))
