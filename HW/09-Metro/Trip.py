0e652b60a15723d1e214fdb03a539fa5cbc15591
hrahadiyan <ahadiyan.hr@gmail.com>
2022-08-09 08:55:07 +0430

    relocate all files to main folder

---
+++ HW/09-Metro/Trip.py
@@ -0,0 +1,24 @@
+import logging
+from User import User
+from menu_func import get_owner
+
+# Logging Setup:
+log_format = "%(asctime)s %(name)s %(levelname)s: %(message)s"
+logging.basicConfig(filename= 'metro.log', filemode= 'a',\
+    level= logging.DEBUG, format= log_format)
+
+class Trip:
+    
+    _trip_records = {}
+    
+    def __init__(self, origin: str, destination: str, fare: int):
+        self.origin = origin
+        self.destination = destination
+        self.fare = fare
+    
+    @classmethod    
+    def record_trip(cls, origin, destination, fare, auth_code):
+        trip_number = len(Trip._trip_records)+1
+        Trip._trip_records[trip_number] = (auth_code, Trip(origin, destination, fare))
+        logging.log(logging.INFO, f"A Trip No.{trip_number} takes by user with Authentication Code of {auth_code}")
+        return True
---
+++ HW/09-Metro/exceptions.py
@@ -0,0 +1,52 @@
+class IDNumberError(ValueError):
+    pass
+
+class PasswordError(ValueError):
+    pass
+
+class PhoneError(ValueError):
+    pass
+
+class InstantiateError(UserWarning):
+    pass
+
+class AccountBalanceError(ValueError):
+    pass
+
+class OwnerInstanceError(TypeError):
+    pass
+
+class AuthenticationCodeError(ValueError):
+    pass
+
+class NotEnoughBalance(ValueError):
+    pass
+
+class IsNotBankAccount(TypeError):
+    pass
+
+class AccountNotExist(LookupError):
+    pass
+
+class DepositeError(Exception):
+    pass
+
+class WithdrawError(Exception):
+    pass
+class TransferingError(Exception):
+    pass
+
+class CardBalanceError(ValueError):
+    pass
+
+class ChargeCardError(Exception):
+    pass
+
+class PayTicketError(Exception):
+    pass
+
+class LimitedCardExpired(Exception):
+    pass
+
+class LimitedNumbersError(ValueError):
+    pass
