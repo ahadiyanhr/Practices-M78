137b9e33fc4a5bec20c2097a574d80814d1e46b7
hrahadiyan <ahadiyan.hr@gmail.com>
2022-08-10 21:42:49 +0430

    feat: final solution

--- HW/10/10-3 (caching).py
+++ HW/10/10-3 (caching).py
@@ -1,24 +1,27 @@
-import os
-# from time import process_time_ns
+from time import time
 from cachetools import cached, TTLCache
 
 
 cache1 = TTLCache(maxsize=1000, ttl=60)
 cache2 = TTLCache(maxsize=1000, ttl=60)
 
-def exec_times(some_function):
+def process_timer(func):
     '''
     decorator for measuring function execuation time
     '''
-    t1 = 0
-    def times(*args, **kwargs):
-        nonlocal t1
-        t1 += 1
-        some_function(*args, **kwargs)
-        return t1
-    return times
+    turn = 0
+    
+    def measure(*args, **kwargs):
+        nonlocal turn
+        turn += 1
+        t1 = time()
+        func(*args, **kwargs)
+        t2 = time()
+        timer = t2-t1
+        return f"The function '{func.__name__}' run in {str(timer)[:8]} sec. This function turn '{turn}' times totally."
+    return measure
 
-@exec_times
+@process_timer
 def fac_without_cache(number):
     '''
     Calculate factorial of an input number.
@@ -29,7 +32,7 @@ def fac_without_cache(number):
       return fac_without_cache(number-1)*number
 
 
-@exec_times
+@process_timer
 @cached(cache1)
 def fac_with_cache(number):
     '''
@@ -40,7 +43,7 @@ def fac_with_cache(number):
     else:
       return fac_with_cache(number-1)*number
 
-@exec_times
+@process_timer
 def fib_without_cache(number):
     '''
     Calculate fibonacci series of an input number.
@@ -50,7 +53,7 @@ def fib_without_cache(number):
     else:
         return fib_without_cache(number-1) + fib_without_cache(number-2)
 
-@exec_times
+@process_timer
 @cached(cache2)
 def fib_with_cache(number):
     '''
@@ -62,39 +65,9 @@ def fib_with_cache(number):
         return fib_with_cache(number-1) + fib_with_cache(number-2)
 
 
-# @decorator_timer
-# def fac_without_cache(number):
-#     fac = factorial(number)
-#     return fac
-
-# @decorator_timer
-# def fib_without(number):
-#     fib = fib_without_cache(number)
-#     return fib
-
-
-# @decorator_timer
-# def fac_with_cache(number):
-#     fac = factorial(number)
-#     return fac
-
-
-# @decorator_timer
-# def fib_with(number):
-#     fib = fib_with_cache(number)
-#     return fib
-
-
-# os.system("cls")
-# print("Elapsed time during the program in microseconds\nFactorial:", fac_without_cache(1000), end = "\n")
-print(fac_with_cache(100))
-print(fac_without_cache(100))
-print(fib_with_cache(10))
-print(fib_without_cache(10))
-
-# print("Fibonacci1:", process_time_ns()-t1)
-
-# print("Elapsed time during the program in microseconds\nFactorial:", fac_with_cache(1000), end = "\n")
-# print("Fibonacci:", fib_with_cache(30))
+print(fac_with_cache(200))
+print(fac_without_cache(200))
+print(fib_with_cache(20))
+print(fib_without_cache(20))
 
 
