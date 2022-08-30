874b845a6116b78f145d603fd82830478a1a4c5e
hrahadiyan <ahadiyan.hr@gmail.com>
2022-08-30 19:38:03 +0430

    feat: add Q13

---
+++ Quiz/13/pyfile.py
@@ -0,0 +1,9 @@
+class Parent:
+    # Encapsulation 
+    
+    def encap(self):
+        pass
+
+
+class Child(Parent): # this is for Inheritance - Child inher attributes and methods from Parent
+    pass
