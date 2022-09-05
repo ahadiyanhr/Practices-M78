acfba24a30ef117679752347dac7ac777dfa7b28
hrahadiyan <ahadiyan.hr@gmail.com>
2022-09-05 15:17:30 +0430

    feat: add create_table func.

--- HW/13/13-2 (FileStore)/core/utils.py
+++ HW/13/13-2 (FileStore)/core/utils.py
@@ -1,7 +1,8 @@
 from configs import LOGGING_SETUP
 from os import name as os_name, system as terminal
 import logging
-
+import psycopg2
+from configs import DB_CONNECTION
 
 def clear():
     terminal('cls' if os_name.lower() == 'nt' else 'clear')
@@ -39,3 +40,53 @@ class Logging:
         
     def LOG(log_level, log_message) -> None:
         logging.log(log_level, log_message)
+
+        
+def create_tables():
+    """ create tables in the PostgreSQL database"""
+    commands = (
+        """ CREATE TABLE IF NOT EXISTS User (
+                user_id SERIAL PRIMARY KEY,
+                first_name VARCHAR(50) NOT NULL,
+                last_name VARCHAR(50) NOT NULL,
+                phone VARCHAR(13) NOT NULL,
+                national_code VARCHAR(10) NOT NULL,
+                age integer NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                is_seller BOOLEAN NOT NULL
+                )
+        """,
+        """
+        other: str, id: int=None
+        CREATE TABLE IF NOT EXISTS Teacher (
+                file_id SERIAL PRIMARY KEY,
+                file_name VARCHAR(255) NOT NULL,
+                date_created DATE NOT NULL,
+                date_modified DATE NOT NULL,
+                seller_id INTEGER NOT NULL,
+                FOREIGN KEY (seller_id)
+                    REFERENCES User (user_id)
+                    ON UPDATE CASCADE ON DELETE CASCADE,
+                other VARCHAR(255),
+        )
+        """)
+    try:
+        DBname = DB_CONNECTION["DBname"]
+        HOST = DB_CONNECTION["HOST"]
+        USER = DB_CONNECTION["USER"]
+        PORT = DB_CONNECTION["PORT"]
+        PASSWORD = DB_CONNECTION["PASSWORD"]
+        conn = psycopg2.connect(DBname, USER, HOST, PORT, PASSWORD)
+        cur = conn.cursor()
+        # create table one by one
+        for command in commands:
+            cur.execute(command)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        Logging.LOG('error', error)
+    finally:
+        if conn is not None:
+            conn.close()
