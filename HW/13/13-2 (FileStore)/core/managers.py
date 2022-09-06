96e2e863302d419b957c75e351afaf47ae6d20c9
hrahadiyan <ahadiyan.hr@gmail.com>
2022-09-06 12:28:42 +0430

    feat: add a rough insert method

--- HW/13/13-2 (FileStore)/core/managers.py
+++ HW/13/13-2 (FileStore)/core/managers.py
@@ -1,6 +1,7 @@
 import psycopg2
 import psycopg2.extras
 from core.models import DBModel
+from users.models import User
 from configs import DB_CONNECTION
 from psycopg2._psycopg import connection, cursor
 
@@ -32,6 +33,28 @@ class DBManager:
         """
             return id of created model instance from table
         """
+        command_no_id = """
+            INSERT INTO User(first_name, last_name, phone, national_code, age, password, is_seller) 
+            VALUES(%s, %s, %s, %s , %s, %s, %s)
+            RETURNING user_id
+            """
+        command_with_id = """
+            INSERT INTO User(user_id, first_name, last_name, phone, national_code, age, password, is_seller) 
+            VALUES(%s, %s, %s, %s, %s , %s, %s, %s)
+            """
+        if model_instance.id is None:
+            self.__get_cursor().execute(command_no_id,\
+                (model_instance.first_name, model_instance.last_name, phone, national_code, age, password, is_seller))
+            row = cur.fetchone()
+            self.id = row[0]
+            cur.close()
+            # commit the changes
+            conn.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
 
     def read(self, model_class: type, pk) -> DBModel:  # get
         """
