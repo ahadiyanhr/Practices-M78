378252a58355afeb4a98af7851da9e514d8c6296
hrahadiyan <ahadiyan.hr@gmail.com>
2022-09-07 15:34:46 +0430

    refractor and update: cursor and execute methods

--- HW/13/13-2 (FileStore)/core/managers.py
+++ HW/13/13-2 (FileStore)/core/managers.py
@@ -14,38 +14,43 @@ logging.config.fileConfig('.\\log_configs.ini', disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
 class DBManager:
+    DBname = DB_CONNECTION["DBname"]
     HOST = DB_CONNECTION["HOST"]
     USER = DB_CONNECTION["USER"]
     PORT = DB_CONNECTION["PORT"]
     PASSWORD = DB_CONNECTION["PASSWORD"]
 
-    def __init__(self, database, user=USER, host=HOST, port=PORT, password=PASSWORD) -> None:
-        self.database = database
+    def __init__(self, dbname=DBname, user=USER, host=HOST, port=PORT, password=PASSWORD) -> None:
+        self.dbname = dbname
         self.user = user
         self.host = host
         self.port = port
         self.password = password
 
         self.conn: connection = \
-            psycopg2.connect(dbname=self.database, user=self.user, host=self.host, port=self.port, password=password)
+            psycopg2.connect(dbname=self.dbname, user=self.user, host=self.host, port=self.port, password=password)
 
     def __del__(self):
         self.conn.close()  # Close the connection on delete
 
     def __get_cursor(self) -> cursor:
         # Changing the fetch output from Tuple to Dict utilizing RealDictCursor cursor factory
-        return self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        return self.conn.cursor()#(cursor_factory=psycopg2.extras.RealDictCursor)
     
     def __close_cursor(self) -> None:
         return self.conn.cursor().close()
     
-    def __get_execute(self, model_instance: DBModel, method: str) -> cursor:
-        command = generate_command(model_instance, method)
+    def __get_execute(self, model_instance: DBModel, method: str) -> (tuple|None):
         
+        command = generate_command(model_instance, method)
+        cur = self.__get_cursor()
+
         if (method == "insert") and (model_instance.id is None):
             if isinstance(model_instance, User):
-                return self.__get_cursor().execute(command, (model_instance.first_name, model_instance.last_name,\
-                    model_instance.phone, model_instance.national_code, model_instance.age, model_instance.password, model_instance.is_seller))
+                cur.execute(command, (model_instance.first_name, model_instance.last_name,\
+                    model_instance.phone, model_instance.national_id, model_instance.age,\
+                        model_instance.password, model_instance.is_seller))
+                return cur.fetchone()
             else:
                 return self.__get_cursor().execute(command, (model_instance.file_name, model_instance.date_created,\
                     model_instance.date_modified, model_instance.seller_id, model_instance.other))
@@ -53,7 +58,7 @@ class DBManager:
         elif method == "insert":
             if isinstance(model_instance, User):
                 return self.__get_cursor().execute(command, (model_instance.id, model_instance.first_name, model_instance.last_name,\
-                    model_instance.phone, model_instance.national_code, model_instance.age, model_instance.password, model_instance.is_seller))
+                    model_instance.phone, model_instance.national_id, model_instance.age, model_instance.password, model_instance.is_seller))
             else:
                 return self.__get_cursor().execute(command, (model_instance.id, model_instance.file_name, model_instance.date_created,\
                     model_instance.date_modified, model_instance.seller_id, model_instance.other))
@@ -61,7 +66,7 @@ class DBManager:
         elif method == "update":
             if isinstance(model_instance, User):
                 return self.__get_cursor().execute(command, (model_instance.first_name, model_instance.last_name, model_instance.phone,\
-                    model_instance.national_code, model_instance.age, model_instance.password, model_instance.is_seller, model_instance.id))
+                    model_instance.national_id, model_instance.age, model_instance.password, model_instance.is_seller, model_instance.id))
             else:
                 return self.__get_cursor().execute(command, (model_instance.file_name, model_instance.date_created,\
                     model_instance.date_modified, model_instance.seller_id, model_instance.other, model_instance.id))
@@ -71,8 +76,8 @@ class DBManager:
             return id of created model instance from table
         """
         try:
-            row = self.__get_execute(model_instance, "insert").fetchone()
-            self.id = row[0] # get id of instance
+            row = self.__get_execute(model_instance, "insert")
+            model_instance.id = row[0] # get id of instance
             self.__close_cursor() # close cursor
             self.conn.commit() # commit the changes
         except (Exception, psycopg2.DatabaseError) as error:
@@ -80,7 +85,7 @@ class DBManager:
         finally:
             if self.conn is not None:
                 self.__del__()
-        return self.id
+        return model_instance.id
 
     def read(self, model_class: type, id: int) -> DBModel:
         """
