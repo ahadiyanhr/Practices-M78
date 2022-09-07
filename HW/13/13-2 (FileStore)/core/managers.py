84fc647c2ebbd6232797448bf9b96307d8266291
hrahadiyan <ahadiyan.hr@gmail.com>
2022-09-07 15:36:49 +0430

    feat: modify update method

--- HW/13/13-2 (FileStore)/core/managers.py
+++ HW/13/13-2 (FileStore)/core/managers.py
@@ -50,28 +50,26 @@ class DBManager:
                 cur.execute(command, (model_instance.first_name, model_instance.last_name,\
                     model_instance.phone, model_instance.national_id, model_instance.age,\
                         model_instance.password, model_instance.is_seller))
-                return cur.fetchone()
             else:
                 cur.execute(command, (model_instance.file_name, model_instance.date_created,\
                     model_instance.date_modified, model_instance.seller_id, model_instance.other))
-                return cur.fetchone()
+            return cur.fetchone()
         
         elif method == "insert":
             if isinstance(model_instance, User):
                 cur.execute(command, (model_instance.id, model_instance.first_name, model_instance.last_name,\
                     model_instance.phone, model_instance.national_id, model_instance.age, model_instance.password, model_instance.is_seller))
-                return cur.fetchone()
             else:
                 cur.execute(command, (model_instance.id, model_instance.file_name, model_instance.date_created,\
                     model_instance.date_modified, model_instance.seller_id, model_instance.other))
-                return cur.fetchone()
+            return cur.fetchone()
         
         elif method == "update":
             if isinstance(model_instance, User):
-                return self.__get_cursor().execute(command, (model_instance.first_name, model_instance.last_name, model_instance.phone,\
+                cur.execute(command, (model_instance.first_name, model_instance.last_name, model_instance.phone,\
                     model_instance.national_id, model_instance.age, model_instance.password, model_instance.is_seller, model_instance.id))
             else:
-                return self.__get_cursor().execute(command, (model_instance.file_name, model_instance.date_created,\
+                cur.execute(command, (model_instance.file_name, model_instance.date_created,\
                     model_instance.date_modified, model_instance.seller_id, model_instance.other, model_instance.id))
 
     def create(self, model_instance: DBModel) -> int:
@@ -114,11 +112,11 @@ class DBManager:
             update instance in db table by get all model_instance attrs
         """
         try:
-            self.__get_execute(model_instance, "updata")
+            self.__get_execute(model_instance, "update")
             self.__close_cursor() # close cursor
             self.conn.commit() # commit the changes
         except (Exception, psycopg2.DatabaseError) as error:
-            Logging.LOG('error', error)
+            logger.error(error)
         finally:
             if self.conn is not None:
                 self.__del__()
