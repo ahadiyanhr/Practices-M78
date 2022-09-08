31945954bb5663331bb0a3f8ee1271554a6436a7
hrahadiyan <ahadiyan.hr@gmail.com>
2022-09-08 19:13:32 +0430

    feat: add USER args and questions

--- HW/13/13-2 (FileStore)/configs.py
+++ HW/13/13-2 (FileStore)/configs.py
@@ -14,3 +14,12 @@ INFO = {
     "version": "1.0.0",
 }
 
+USER = {
+    "First Name": "Only Words",
+    "Last Name": "Only Words",
+    "Phone Number": "A Valid Phone Number that starts with 09../+989..",
+    "National ID Number": "A Number with length of 10",
+    "User's Age": "A Number between 1-99",
+    "Password": "Anything!",
+    "User is a Seller": "User is a Seller or not"
+}
