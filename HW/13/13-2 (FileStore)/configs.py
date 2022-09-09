a35803b780c30c8647efe36e20b3f2d6ec956783
hrahadiyan <ahadiyan.hr@gmail.com>
2022-09-09 21:34:28 +0430

    feat: update questions dict

--- HW/13/13-2 (FileStore)/configs.py
+++ HW/13/13-2 (FileStore)/configs.py
@@ -14,12 +14,21 @@ INFO = {
     "version": "1.0.0",
 }
 
-USER = {
-    "First Name": "Only Words",
-    "Last Name": "Only Words",
-    "Phone Number": "A Valid Phone Number that starts with 09../+989..",
-    "National ID Number": "A Number with length of 10",
-    "User's Age": "A Number between 1-99",
-    "Password": "Anything!",
-    "User is a Seller": "User is a Seller or not"
+Questions = {
+    "user": {
+        "First Name": "Only Words",
+        "Last Name": "Only Words",
+        "Phone Number": "A Valid Phone Number that starts with 09../+989..",
+        "National ID Number": "A Number with length of 10",
+        "User's Age": "A Number between 1-99",
+        "Password": "Anything!",
+        "User is a Seller": "User is a Seller or not"
+    },
+    "file": {
+        "File Name": "Anything!",
+        "Data Created": "YYYY-MM-DD",
+        "Data Modified": "YYYY-MM-DD",
+        "Seller ID": "ID of user that is Seller",
+        "Other": "Any information about the File"  
+    }
 }
